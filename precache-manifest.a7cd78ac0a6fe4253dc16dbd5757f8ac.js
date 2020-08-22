489511b2e66e054b4fc49aaba706a0b3b1ef3425
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-08-22 14:38:11 -0700

    Updates

---
+++ precache-manifest.a7cd78ac0a6fe4253dc16dbd5757f8ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ef9cbd1d17771d92721b1fd2ddc955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a0585e5437d83adecc",
+    "url": "/static/css/main.bd329325.chunk.css"
+  },
+  {
+    "revision": "f0d60595e61b98e9a72a",
+    "url": "/static/js/2.cd32a467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd32a467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a0585e5437d83adecc",
+    "url": "/static/js/main.f9c90b50.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "2a6937c45a887147aecbd565cbfff826",
+    "url": "/static/media/me.2a6937c4.png"
+  },
+  {
+    "revision": "5fc8d93f3d84a2bc17b2434770158f4a",
+    "url": "/static/media/myself_waterbackground.5fc8d93f.png"
+  },
+  {
+    "revision": "63ff3d04a5ca9ef44d6a8d708efb1395",
+    "url": "/static/media/water-rotated.63ff3d04.jpg"
+  },
+  {
+    "revision": "cf45750187e9d57d649d580656c9eeb2",
+    "url": "/static/media/water.cf457501.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.70329969db0aed2949d03eccb168d6a7.js"
+  "/precache-manifest.a7cd78ac0a6fe4253dc16dbd5757f8ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
